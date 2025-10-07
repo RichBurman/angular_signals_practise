c1847e45061b4468fb8684558e0a60526e72db4d
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-07 14:59:45 +0100

    Refactor task list component to improve structure and add missing Angular Material modules

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import { MatButtonModule, MatIconButton } from '@angular/material/button';
 import { TaskService } from '../../services/task.service';
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatCardModule],
+  imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatCardModule, MatCheckboxModule, MatIconModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
